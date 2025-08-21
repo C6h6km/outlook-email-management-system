ce68ecce28a9af0f6d50bee2b8d37966f0b80e3a
C6h6Km <bdkm656@gamil.com>
2025-08-21 16:55:25 +0800

    chore(vercel): migrate to Node.js Runtime and set runtime=nodejs22.x for api/server

--- api/server.js
+++ api/server.js
@@ -3,3 +3,9 @@ const app = require('../server');
 // Export the Express app as the serverless function handler
 module.exports = app;
 
+
+
+// Vercel Node.js Runtime config (use Node 22)
+module.exports.config = {
+  runtime: 'nodejs22.x'
+};
